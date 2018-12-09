dbba379fa1faa600ab8a047907df6aa2707e87d5
fastNick88 <fastNick88@gmail.com>
2018-12-09 16:45:02 +0200

    Update gcd function as recursive one

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	npm install
 
 start:
-	npx babel-node -- src/bin/brain-arithmetic-progression.js
+	npx babel-node -- src/bin/brain-gcd.js
 
 publish:
 	npm publish
--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,24 +4,17 @@ const description = '\nFind the greatest common divisor of given numbers.';
 
 const maxnumberForRandomvalue = 20;
 
-const getGCD = (firstNumber, secondNumber) => {
-  let absFirstNumber = Math.abs(firstNumber);
-  let absSecondNumber = Math.abs(secondNumber);
-  if (absSecondNumber > absFirstNumber) {
-    const temp = absFirstNumber; absFirstNumber = absSecondNumber; absSecondNumber = temp;
+const gcdRec = (firstNumber, secondNumber) => {
+  if (secondNumber) {
+    return gcdRec(secondNumber, firstNumber % secondNumber);
   }
-  for (;;) {
-    if (absSecondNumber === 0) return absFirstNumber;
-    absFirstNumber %= absSecondNumber;
-    if (absFirstNumber === 0) return absSecondNumber;
-    absSecondNumber %= absFirstNumber;
-  }
+  return Math.abs(firstNumber);
 };
 
 export const getQuestionAnswerPair = () => {
   const firstNumber = gameEngine.generateRandomNumber(0, maxnumberForRandomvalue);
   const secondNumber = gameEngine.generateRandomNumber(0, maxnumberForRandomvalue);
-  const gcd = getGCD(firstNumber, secondNumber);
+  const gcd = gcdRec(firstNumber, secondNumber);
   return { question: (`${firstNumber} ${secondNumber}`), answer: gcd };
 };
 
